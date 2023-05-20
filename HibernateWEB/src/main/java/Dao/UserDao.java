e10794f433fb334ded6acaac609379428d045ee5
darshankalathiya9 <darshankalathiya963@gmail.com>
2023-05-20 10:37:18 +0530

    HibernateWEB Update Query Done

--- HibernateWEB/src/main/java/Dao/UserDao.java
+++ HibernateWEB/src/main/java/Dao/UserDao.java
@@ -35,68 +35,68 @@ public class UserDao {
 		Query q = session.createQuery("from User u where u.Email=:Email and u.Password=:Password");
 		q.setParameter("Email", u.getEmail());
 		q.setParameter("Password", u.getPassword());
-		
+
 		list = q.list();
 		u1 = list.get(0);
-		
+
 		tx.commit();
 		session.close();
 		return u1;
 	}
-	
-	public List<User> getAllUser(){
+
+	public List<User> getAllUser() {
 		list = new ArrayList<User>();
-		session =  new DBConnection().getSession();
+		session = new DBConnection().getSession();
 		tx = session.beginTransaction();
-		
+
 		Query q = session.createQuery("from User");
 		list = q.list();
-		
+
 		tx.commit();
 		session.close();
 		return list;
 	}
-	
+
 	public User getUserByID(int ID) {
 		User u1 = null;
 		session = new DBConnection().getSession();
 		tx = session.beginTransaction();
-		
+
 		Query q = session.createQuery("from User u where u.ID=:ID");
 		q.setParameter("ID", ID);
 		list = q.list();
 		u1 = list.get(0);
-		
+
 		tx.commit();
 		session.close();
 		System.out.println(u1);
 		return u1;
 	}
-	
+
 	public void updateUser(User u) {
 		session = new DBConnection().getSession();
 		tx = session.beginTransaction();
-		
-		Query q = session.createQuery("update User u set u.Name=:Name, u.Contact=:Contact, u.Address=:Address, u.Email=:u.Email, u.Password=:Password where u.ID:=ID");
-		q.setParameter("Name", u.getName());
-		q.setParameter("Contact", u.getContact());
-		q.setParameter("Address", u.getAddress());
-		q.setParameter("Email", u.getEmail());
-		q.setParameter("Password", u.getPassword());
-		q.setParameter("ID", u.getID());
-		
-		list = q.list();
-		u = list.get(0);
-		
+
+		Query q = session.createQuery("update User u set u.Name=?1, u.Contact=?2, u.Address=?3, u.Email=?4, u.Password=?5 where u.ID=?6");
+		q.setParameter(1, u.getName());
+		q.setParameter(2, u.getContact());
+		q.setParameter(3, u.getAddress());
+		q.setParameter(4, u.getEmail());
+		q.setParameter(5, u.getPassword());
+		q.setParameter(6, u.getID());
+		q.executeUpdate();
+
 		tx.commit();
 		session.close();
 		System.out.println("Data Updated succesfully.");
 	}
-	
+
 	public void deleteUser(User u) {
 		session = new DBConnection().getSession();
 		tx = session.beginTransaction();
+
 		session.delete(u);
+
 		tx.commit();
 		session.close();
 		System.out.println("Data Deleted Succesfully.");
